f62d298d23d228113fd35e8017ab857e312e754a
shivalkarrahul <rahulshivalkar@rediffmail.com>
2024-01-11 19:34:44 +0530

    Update server.js to test webhook

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello, Welcome to the sample application developed, tested and deployed using How to integrate Jenkins with GitHub blog");
+    response.end("Hello, Welcome to the sample application developed, tested and deployed using How to integrate Jenkins with GitHub blog": Test Webhook);
 
 });
 
